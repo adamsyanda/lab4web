c2c0255183d2be3a171a74f96bd78149ac804d46
unknown <adamsuryaananda29@gmail.com>
2023-04-03 18:37:18 +0700

    File Ditambahkan

---
+++ lab4_php_modular/kontak.php
@@ -0,0 +1,13 @@
+<?php require('header.php'); ?>
+<div class="container">
+    <h2>Kontak</h2>
+    <div class="box">
+        <p>Nama : Adam surya ananda</p>
+        <p>Nim : 312110297</p>
+        <p>Kelas : TI.21.A.1</p>
+        <p>Matkul : Pemograman Web 2</p>
+        <p>Email : adamsuryaananda29@gmail.com</p>
+        <p>No. Telp : 08976346783493</p>
+    </div>
+</div>
+<?php require('footer.php'); ?>
